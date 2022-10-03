a246dbd048807f870a94f7b34a21790e40d10e75
ParasSharma <parassharma3624@gmail.com>
2022-10-03 22:48:47 +0530

    guess the number

--- Python/guessthenumber.py
+++ Python/guessthenumber.py
@@ -1,10 +1,20 @@
 import random
 
 num = random.randint(0,9)
-guessNum = int(input("Enter number between 0 and 9: "))
+guessNum = int(input("Enter number between 0 and 9: ")) #Guessing the number
 
 while guessNum != num:
-    print("Number not matched")
+    print("Number not matched")       #If number does not match
     guessNum = int(input("Enter number between 0 and 9: "))
 
-print("\nCongratulations, you number matched")
+print("\nCongratulations, you number matched")  #If the number match
+
+#Output
+
+#Enter number between 0 and 9: 4
+#Number not matched
+#Enter number between 0 and 9: 3
+#Number not matched
+#Enter number between 0 and 9: 7
+
+#Congratulations, you number matched
