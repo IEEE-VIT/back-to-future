2ec6cc01dad706834a838dd58c2dc742a2f6aa8f
tommyrachmadiono <tommyrachmadiono@gmail.com>
2022-10-03 21:06:08 +0700

    fix: implementation java algorithm to check prime number

--- Java/CheckPrime.java
+++ Java/CheckPrime.java
@@ -1,22 +1,26 @@
+
 // Check if the number is prime or not using java
-
 public class CheckPrime {
-    // Number to be checked
-    static int num = 73;
+  // Number to be checked
+  static int num = 73;
 
-    public static void main(String[] args) {
-        // Check if the number is prime or not
-        System.out.println(checkPrime(num));
-    }
-
+  public static void main(String[] args) {
     // Check if the number is prime or not
-    static boolean checkPrime(int num) {
-        // Check if the number is prime or not
-        // your code here
-        // return true or false
-        return true; // overwrite it according to conditions
+    System.out.println(checkPrime(num));
+  }
 
-    }
+  // Check if the number is prime or not
+  static boolean checkPrime(int num) {
+    // Check if the number is prime or not
+    // your code here
+    if (num <= 1) return false;
+
+    for (int i = 2; i < num; i++)
+      if (num % i == 0)
+        return false;
+
+    return true;
+  }
 
 }
 
