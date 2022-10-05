bb658dd33fc90afd82274f1883afdac2c558e030
Nishchay Sinha <nishchay.space@gmail.com>
2022-10-05 17:19:33 +0530

    Implemented some scripts in rust

--- Rust/heapsort.rs
+++ Rust/heapsort.rs
@@ -1,16 +1,89 @@
-// Heap Sort in Rust
+pub fn heap_sort<T: Ord>(arr: &mut [T]) {
+    if arr.len() <= 1 {
+        return; // already sorted
+    }
 
-fn main() {
-    let mut arr = [76, 22, 21, 96, 70, 20];
-    heap_sort(&mut arr);
-    println!("{:?}", arr);
+    heapify(arr);
+
+    for end in (1..arr.len()).rev() {
+        arr.swap(0, end);
+        move_down(&mut arr[..end], 0);
+    }
 }
 
-fn heap_sort(arr: &mut [i32]) {
-    // your code goes here
-    // print the sorted array
+
+fn heapify<T: Ord>(arr: &mut [T]) {
+    let last_parent = (arr.len() - 2) / 2;
+    for i in (0..=last_parent).rev() {
+        move_down(arr, i);
+    }
 }
 
-// Output:
 
-// [20, 21, 22, 70, 76, 96]
+fn move_down<T: Ord>(arr: &mut [T], mut root: usize) {
+    let last = arr.len() - 1;
+    loop {
+        let left = 2 * root + 1;
+        if left > last {
+            break;
+        }
+        let right = left + 1;
+        let max = if right <= last && arr[right] > arr[left] {
+            right
+        } else {
+            left
+        };
+
+        if arr[max] > arr[root] {
+            arr.swap(root, max);
+        }
+        root = max;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let mut arr: Vec<i32> = Vec::new();
+        heap_sort(&mut arr);
+        assert_eq!(&arr, &[]);
+    }
+
+    #[test]
+    fn single_element() {
+        let mut arr = vec![1];
+        heap_sort(&mut arr);
+        assert_eq!(&arr, &[1]);
+    }
+
+    #[test]
+    fn sorted_array() {
+        let mut arr = vec![1, 2, 3, 4];
+        heap_sort(&mut arr);
+        assert_eq!(&arr, &[1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn unsorted_array() {
+        let mut arr = vec![3, 4, 2, 1];
+        heap_sort(&mut arr);
+        assert_eq!(&arr, &[1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn odd_number_of_elements() {
+        let mut arr = vec![3, 4, 2, 1, 7];
+        heap_sort(&mut arr);
+        assert_eq!(&arr, &[1, 2, 3, 4, 7]);
+    }
+
+    #[test]
+    fn repeated_elements() {
+        let mut arr = vec![542, 542, 542, 542];
+        heap_sort(&mut arr);
+        assert_eq!(&arr, &vec![542, 542, 542, 542]);
+    }
+}
