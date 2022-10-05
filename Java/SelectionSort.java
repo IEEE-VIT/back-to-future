f3ec6a018a8f94465073233b6ee4c4dbfba39bbd
rutuja1121 <65657589+rutuja1121@users.noreply.github.com>
2022-10-05 20:19:41 +0530

    implementation of selectionSort using java (#32)
    Co-authored-by: rutuja1121 <2019rutuja.patil@ves.ac.in>

--- Java/SelectionSort.java
+++ Java/SelectionSort.java
@@ -13,7 +13,26 @@ public class SelectionSort {
     static void selectionSort(int[] arr) {
         // Selection Sort
         // your code here
+        int n = arr.length;
+ 
+        for (int i = 0; i < n-1; i++)
+        {
+        
+            int min_idx = i;
+            for (int j = i+1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+ 
+           
+         
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
         // print the array
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
 
     }
 
