90864449b6a0493c25e33182e33e2df10a7309f9
Naman Tiwari <namantiwari2002@gmail.com>
2022-10-05 22:45:24 +0530

    Added bubble sort code

--- Javascript/bubblesort.js
+++ Javascript/bubblesort.js
@@ -1,7 +1,14 @@
 // Bubble Sort in JavaScript
 
 function bubbleSort(arr) {
-    // your code here
+    for (let i = arr.length - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+          if (arr[j] > arr[j + 1]) {
+            [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+          }
+        }
+      }
+      return arr
 }
 
 var arr = [76, 22, 21, 96, 70, 20];
