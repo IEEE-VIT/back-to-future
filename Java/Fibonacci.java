f98be972a29fb3c4dc06373296b899c7bcc2e111
Ashpreetwadhwa <102176527+Ashpreetwadhwa@users.noreply.github.com>
2022-10-06 18:58:55 +0530

    Added fibonacci in java

--- Java/Fibonacci.java
+++ Java/Fibonacci.java
@@ -1,5 +1,6 @@
 // Fibonacci sequence in Java
 
+
 public class Fibonacci {
 
     public static void main(String[] args) {
@@ -9,8 +10,17 @@ public class Fibonacci {
     }
 
     static void fib(int n) {
-        // Fibonacci sequence
-        // your code here
+       int  firstTerm = 0, secondTerm = 1;
+        System.out.println("Fibonacci Series till " + n + " terms:");
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + ", ");
+
+            // compute the next term
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
 
     }
 
