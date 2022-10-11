722b5652db69b52e2727830b1360d7da9fec68c7
David <dav.gruber@htlstp.at>
2022-10-11 14:41:59 +0200

    add: selectionsort-c

--- C/selectionsort.c
+++ C/selectionsort.c
@@ -1,11 +1,34 @@
 // Selection Sort in C
 
 #include <stdio.h>
+#include <math.h>
 
 void selectionSort(int arr[], int n)
 {
-    // your code goes here
-    // print the sorted array
+    int index = 0;
+    int min;
+    for (int i = 0; i < n; i++)
+    {
+        min = arr[i];
+        for (int j = i; j < n; j++)
+        {
+            if (arr[j] < min)
+            {
+                index = j;
+                min = arr[index];
+            }
+        }
+        if (arr[i] != arr[index])
+        {
+            arr[i] += arr[index];
+            arr[index] = arr[i] - arr[index];
+            arr[i] -= arr[index];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 }
 
 int main()
