84b410fa2b6c5398a1f93574d6d0462974f6ede2
Shreya <shreya.maity02@gmail.com>
2022-10-15 08:55:37 +0530

    add : checkprime

--- C/checkprime.c
+++ C/checkprime.c
@@ -6,6 +6,12 @@ int IsPrime(int n)
 {
     // your code goes here
     // return 1 if n is prime else return 0
+    int i;
+    for(i=2;i<=n/2;i++){
+        if(n%i==0)
+           return 0;
+    }
+    return 1;
 }
 int main()
 {
