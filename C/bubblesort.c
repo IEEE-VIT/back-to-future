46a50dbffa3bacfb4dc273904091c63d4eff62b5
Shreya <shreya.maity02@gmail.com>
2022-10-15 09:12:31 +0530

    add : bubblesort

--- C/bubblesort.c
+++ C/bubblesort.c
@@ -6,6 +6,18 @@ void bubbleSort(int arr[], int n)
 {
     // your code goes here
     // print the sorted array
+    int i,j,t;
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                t=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=t;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+      printf("%d ",arr[i]);
 }
 
 int main()
