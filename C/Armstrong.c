7adb3df52355727400c8cd5d6bd998cc120282bf
Parth Dodia <hashtag7532@gmail.com>
2022-10-17 12:20:22 +0530

    C Code for Armstrong number

---
+++ C/Armstrong.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int n,r,sum=0,temp;
+    printf("Enter the number = ");
+    scanf("%d",&n);
+    temp=n;
+    while(n>0){
+        r=n%10;
+        sum=sum+(r*r*r);
+        n=n/10;
+    }
+    if (temp==sum){
+        printf("Armstrong number");
+    }
+
+    else{
+        printf("Not an Armstrong number");
+    }
+}
