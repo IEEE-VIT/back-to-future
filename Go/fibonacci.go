877bcf70ba7aaf3c9a17612fdbbd3ce6cc489cb6
feniljain <49019259+feniljain@users.noreply.github.com>
2022-10-30 23:59:29 +0530

    feat: fibonacci for golang (#82)

--- Go/fibonacci.go
+++ Go/fibonacci.go
@@ -2,6 +2,8 @@
 
 package main
 
+import "fmt"
+
 func main() {
 	var n int = 10
 	fibonacci(n)
@@ -9,7 +11,20 @@ func main() {
 
 func fibonacci(n int) {
 	// your code here
+	a := 0
+	b := 1
+	c := 1
 
+	fmt.Print(a, " ")
+	fmt.Print(b, " ")
+	for i := 0; i < n-2; i++ {
+		c = a + b
+
+		fmt.Print(c, " ")
+
+		a = b
+		b = c
+	}
 }
 
 // Output:
